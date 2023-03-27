13ef7adf11742e8fab1985a7cf09047190088d24
Irony <1955502576@qq.com>
2023-03-27 23:01:12 +0800

    009 保护模式和全局描述符

--- src/loader.asm
+++ src/loader.asm
@@ -5,7 +5,7 @@ dw 0x55aa
 
 mov si, loading
 call print
-xchg bx, bx
+
 detect_memory:
     xor ebx, ebx
     mov ax, 0
@@ -27,22 +27,24 @@ detect_memory:
 
     mov si, detecting
     call print
-
     xchg bx, bx
+    mov byte [0xb8000], 'P'
+    jmp prepare_protected_mode
+    
 
-    ;结构体数量
-    mov ecx, [ards_count]
-    ;结构体指针
-    mov si, 0
-.show:
-    mov eax, [ards_buffer+si]
-    mov ebx, [ards_buffer+si+8]
-    mov edx, [ards_buffer+si+16]
-    add si, 20
+prepare_protected_mode:
     xchg bx, bx
-    loop .show
+    cli
+    ;打开A20线
+    in al, 0x92
+    or al, 0b10
+    out 0x92, al
+    lgdt [gdt_ptr]
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+    jmp dword code_selector:protect_mode
 
-jmp $
 
 print:
     mov ah, 0x0e
@@ -67,6 +69,54 @@ error:
     hlt ;让cpu停止
     jmp $
     .msg db "Loading Error!!!", 10, 13, 0
+
+[bits 32]
+protect_mode:
+    xchg bx, bx
+    mov ax, data_selector
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    mov esp, 0x10000
+
+    mov byte [0xb8000], 'P'
+
+    mov byte [0x200000], 'P'
+
+    jmp $
+
+
+    
+
+code_selector equ (1<<3)
+data_selector equ (2<<3)
+
+memory_base equ 0
+memory_limit equ (((1024*1024*1024*4)/(1024*4))-1)
+
+gdt_ptr:
+    dw (gdt_end-gdt_base)-1
+    dd gdt_base
+gdt_base:
+    dd 0, 0
+gdt_code:
+    dw memory_limit & 0xffff
+    dw memory_base & 0xffff
+    db (memory_base>>16) & 0xff
+    db 0b1_00_1_1_0_1_0
+    db 0b1_1_0_0_0000 | ((memory_limit>>16) & 0xf)
+    db (memory_base>>24) & 0xff
+gdt_data:
+    dw memory_limit & 0xffff
+    dw memory_base & 0xffff
+    db (memory_base>>16) & 0xff
+    db 0b1_00_1_0_0_1_0
+    db 0b1_1_0_0_0000 | ((memory_limit>>16) & 0xf)
+    db (memory_base>>24) & 0xff
+gdt_end:
 
 ards_count:
     dw 0
