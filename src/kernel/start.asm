ea0cea25cae762f8f9d47cb46636ad2f0280744f
Irony <1955502576@qq.com>
2023-03-30 15:00:17 +0800

    update .gitignore

--- src/kernel/start.asm
+++ src/kernel/start.asm
@@ -5,7 +5,7 @@ extern kernel_init
 global _start
 _start:
     ; mov byte [0xb8000], 'K' 
-    xchg bx, bx
+    ; bochs的魔术断点可以去掉了，因为用的是qemu
+    ; xchg bx, bx
     call kernel_init
-    xchg bx, bx
     jmp $
