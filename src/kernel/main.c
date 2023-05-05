35aade97da1b7ef24f09133936a2b0f7931bad27
Irony <1955502576@qq.com>
2023-05-05 16:49:33 +0800

    038 实时时钟

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -2,6 +2,8 @@ extern void console_init();
 extern void gdt_init();
 extern void interrupt_init();
 extern void clock_init();
+extern void time_init();
+extern void rtc_init();
 extern void hang();
 
 
@@ -11,7 +13,9 @@ void kernel_init()
     gdt_init();
     interrupt_init();
     // task_init();
-    clock_init();
+    // clock_init();
+    time_init();
+    rtc_init();
     asm volatile("sti");
     hang();
     return;
--- src/kernel/start.asm
+++ src/kernel/start.asm
@@ -4,11 +4,5 @@ extern kernel_init
 
 global _start
 _start:
-    ; mov byte [0xb8000], 'K' 
-    ; bochs的魔术断点可以去掉了，因为用的是qemu
-    ; xchg bx, bx
     call kernel_init
-    int 0x80
-    ; mov bx, 0
-    ; div bx
     jmp $
