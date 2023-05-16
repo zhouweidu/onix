b381005c043847d277a2bd87e751a9abb46d2d9e
Irony <1955502576@qq.com>
2023-05-16 15:29:20 +0800

    063 进入用户模式

--- src/kernel/task.c
+++ src/kernel/task.c
@@ -7,6 +7,7 @@
 #include <onix/string.h>
 #include <onix/bitmap.h>
 #include <onix/syscall.h>
+#include <onix/global.h>
 
 #define NR_TASKS 64
 
@@ -14,6 +15,7 @@ extern u32 volatile jiffies;
 extern u32 jiffy;
 extern bitmap_t kernel_map;
 extern void task_switch(task_t *next);
+extern tss_t tss;
 
 static task_t *task_table[NR_TASKS]; // 任务表
 static list_t block_list;            // 任务默认阻塞链表
@@ -151,6 +153,17 @@ void task_wakeup()
     }
 }
 
+// 激活任务
+void task_activate(task_t *task)
+{
+    assert(task->magic == ONIX_MAGIC);
+
+    if (task->uid != KERNEL_USER)
+    {
+        tss.esp0 = (u32)task + PAGE_SIZE;
+    }
+}
+
 task_t *running_task()
 {
     asm volatile(
@@ -180,6 +193,7 @@ void schedule()
     if (next == current)
         return;
 
+    task_activate(next);
     task_switch(next);
 }
 
@@ -212,6 +226,47 @@ static task_t *task_create(target_t target, const char *name, u32 priority, u32 
     return task;
 }
 
+// 调用该函数的地方不能有任何局部变量
+// 调用前栈顶需要准备足够的空间
+void task_to_user_mode(target_t target)
+{
+    task_t *task = running_task();
+
+    u32 addr = (u32)task + PAGE_SIZE;
+
+    addr -= sizeof(intr_frame_t);
+    intr_frame_t *iframe = (intr_frame_t *)(addr);
+
+    iframe->vector = 0x20;
+    iframe->edi = 1;
+    iframe->esi = 2;
+    iframe->ebp = 3;
+    iframe->esp_dummy = 4;
+    iframe->ebx = 5;
+    iframe->edx = 6;
+    iframe->ecx = 7;
+    iframe->eax = 8;
+
+    iframe->gs = 0;
+    iframe->ds = USER_DATA_SELECTOR;
+    iframe->es = USER_DATA_SELECTOR;
+    iframe->fs = USER_DATA_SELECTOR;
+    iframe->ss = USER_DATA_SELECTOR;
+    iframe->cs = USER_CODE_SELECTOR;
+
+    iframe->error = ONIX_MAGIC;
+
+    u32 stack3 = alloc_kpage(1);
+
+    iframe->eip = (u32)target;
+    iframe->eflags = (0 << 12 | 0b10 | 1 << 9);
+    // esp指向的是用户栈，stack3是用户栈，task+PAGE_SIZE是内核栈
+    iframe->esp = stack3 + PAGE_SIZE;
+    asm volatile(
+        "movl %0, %%esp\n"
+        "jmp interrupt_exit\n" ::"m"(iframe));
+}
+
 static void task_setup()
 {
     task_t *task = running_task();
@@ -224,6 +279,7 @@ static void task_setup()
 extern void idle_thread();
 extern void init_thread();
 extern void test_thread();
+extern void real_init_thread();
 
 void task_init()
 {
@@ -232,5 +288,5 @@ void task_init()
     task_setup();
     idle_task = task_create(idle_thread, "idle", 1, KERNEL_USER);
     task_create(init_thread, "init", 5, NORMAL_USER);
-    task_create(test_thread, "test", 5, NORMAL_USER);
+    task_create(test_thread, "test", 5, KERNEL_USER);
 }
--- src/kernel/thread.c
+++ src/kernel/thread.c
@@ -3,6 +3,7 @@
 #include <onix/debug.h>
 #include <onix/mutex.h>
 #include <onix/printk.h>
+#include <onix/task.h>
 
 void idle_thread()
 {
@@ -21,19 +22,33 @@ void idle_thread()
 
 extern u32 keyboard_read(char *buf, u32 count);
 
-void init_thread()
+static void real_init_thread()
 {
-    set_interrupt_state(true);
-    u32 counter = 0;
+    //0010 0011
+    u32 counter=0;
     char ch;
     while (true)
     {
-        bool intr=interrupt_disable();
-        keyboard_read(&ch, 1);
-        printk("%c",ch);
-        // LOGK("init task %d...\n", counter++);
-        set_interrupt_state(intr);
+        ;
     }
+    
+}
+
+void init_thread()
+{
+    // set_interrupt_state(true);
+    // u32 counter = 0;
+    // char ch;
+    // while (true)
+    // {
+    //     bool intr=interrupt_disable();
+    //     keyboard_read(&ch, 1);
+    //     printk("%c",ch);
+    //     // LOGK("init task %d...\n", counter++);
+    //     set_interrupt_state(intr);
+    // }
+    char temp[100];
+    task_to_user_mode(real_init_thread);
 }
 
 void test_thread()
