671c342821a5aaa14f2157787bde17a7a8748775
irony_9w <1955502576@qq.com>
2023-10-22 16:01:01 +0800

    143 基础网络配置

--- src/kernel/test.c
+++ src/kernel/test.c
@@ -6,17 +6,20 @@
 
 err_t sys_test()
 {
-    LOGK("test syscall. .. \n");
-    
-    cpu_vendor_t vendor;
-    cpu_vendor_id(&vendor);
-    printk("CPU vendor id: %s\n", vendor.info);
-    printk("CPU maxvalue:0x%X\n", vendor.max_value);
+    // LOGK("test syscall. .. \n");
 
-    cpu_version_t ver;
-    cpu_version(&ver);
-    printk("FPU support state: %d\n", ver.FPU);
-    printk("APIC support state:  %d\n", ver.APIC);
+    // cpu_vendor_t vendor;
+    // cpu_vendor_id(&vendor);
+    // printk("CPU vendor id: %s\n", vendor.info);
+    // printk("CPU maxvalue:0x%X\n", vendor.max_value);
 
+    // cpu_version_t ver;
+    // cpu_version(&ver);
+    // printk("FPU support state: %d\n", ver.FPU);
+    // printk("APIC support state:  %d\n", ver.APIC);
+
+    LOGK("%08b\n", 0x5a);
+    LOGK("%m\n", "\x11\x22\x03\x04\x5f\x5a");
+    LOGK("%r\n", "\xff\x4e\x03\x04");
     return EOK;
 }
--- src/utils/cmd.mk
+++ src/utils/cmd.mk
@@ -15,6 +15,7 @@ QEMU+= -chardev stdio,mux=on,id=com1 # 字符设备 1
 # QEMU+= -chardev udp,id=com2,port=7777,ipv4=on # 字符设备 2
 QEMU+= -serial chardev:com1 # 串口 1
 # QEMU+= -serial chardev:com2 # 串口 2
+QEMU+= -netdev tap,id=eth0,ifname=tap0,script=no,downscript=no # 网络设备
 
 QEMU_DISK := -drive file=$(BUILD)/master.img,if=ide,index=0,media=disk,format=raw # 主硬盘
 QEMU_DISK += -drive file=$(BUILD)/slave.img,if=ide,index=1,media=disk,format=raw # 从硬盘
@@ -24,11 +25,11 @@ QEMU_DISK_BOOT:= -boot c
 QEMU_DEBUG:= -s -S
 
 .PHONY: qemu
-qemu: $(IMAGES)
+qemu: $(IMAGES) $(BR0) $(TAPS)
 	$(QEMU) $(QEMU_DISK) $(QEMU_DISK_BOOT)
 
 .PHONY: qemug
-qemug: $(IMAGES)
+qemug: $(IMAGES) $(BR0) $(TAPS)
 	$(QEMU) $(QEMU_DISK) $(QEMU_DISK_BOOT) $(QEMU_DEBUG)
 
 $(BUILD)/master.vmdk: $(BUILD)/master.img
